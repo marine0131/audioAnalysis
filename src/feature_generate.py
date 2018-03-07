cfbb6431dda040b7cec64b5f757edc2dc661945c
Haijun Wang <wanghj@woosiyuan.com>
2018-03-07 12:02:31 +0800

    update

--- src/feature_generate.py
+++ src/feature_generate.py
@@ -14,14 +14,16 @@ if __name__ == "__main__":
     ff = []
     try:
         if os.path.isdir(sys.argv[1]):
-            ff = [os.path.join(sys.argv[1], f) for f in os.listdir(sys.argv[1]) if f.split('.')[1]=='mp3']
-        feature_file = './feature_data/' + sys.argv[2]
+            # ff = [os.path.join(sys.argv[1], f) for f in os.listdir(sys.argv[1]) if f.split('.')[1]=='mp3']
+            ff = [os.path.join(sys.argv[1], f) for f in os.listdir(sys.argv[1])]
+        feature_file = '../feature_data/' + sys.argv[2]
     except Exception as e:
         print(e)
 
     feat = {}
     with open(feature_file, 'w') as f_file:
         for f in ff:
+            print('processing: {}'.format(f))
             feat['feature'] = list(feature_extraction.feature(f))
             feat_json = json.dumps(feat)
             f_file.write(feat_json+'\n')
